3923f179f80d7cd8f60c4b02a4148318af2b30e6
Griffin T <griffin_git@thommo.io>
2025-02-24 19:47:00 +0800

    1.19.1 added uuid back to login start.

--- src/versions/v760/packets/login.rs
+++ src/versions/v760/packets/login.rs
@@ -10,4 +10,26 @@ pub use prev::s2c;
 // Serverbound
 //
 
-pub use prev::c2s;
+pub mod c2s {
+    use crate::packet::prelude::*;
+
+    impl_packets_enum![LoginStart, EncryptionResponse, LoginPluginResponse];
+
+    #[derive(Debug, Packet, BufType)]
+    #[packet(id = 0x00)]
+    pub struct LoginStart {
+        pub username: String,
+        pub signature_data: Option<login_start::SignatureData>,
+        pub uuid: Option<Uuid>,
+    }
+
+    pub use super::prev::c2s::login_start;
+
+    // 0x01
+    pub use super::prev::c2s::EncryptionResponse;
+
+    pub use super::prev::c2s::encryption_response;
+
+    // 0x02
+    pub use super::prev::c2s::LoginPluginResponse;
+}
